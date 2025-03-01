39087399164c0ccbb3c347154f825d27d891a39b
emily-li <emily.li@live.co.uk>
2025-02-28 20:42:41 -0800

    Add cors

--- src/app.py
+++ src/app.py
@@ -1,11 +1,20 @@
 from flask import Flask
+from flask_cors import CORS
 from routes import setup_routes
 
 app = Flask(__name__)
+CORS(
+    app,
+    resources={
+        r"/api/v1/idea": {
+            "origins": ["http://localhost:*", "https://emily-li.github.io"]
+        }
+    },
+)
 
 # Middleware can be added here
 
 setup_routes(app)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
